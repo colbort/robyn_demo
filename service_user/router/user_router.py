156a430f4e1904f5689886a5588d35ca015a5521
sky <sky-devops@liongaming.co>
2025-02-14 22:01:59 +0800

    修改初始化

--- service_user/router/user_router.py
+++ service_user/router/user_router.py
@@ -1,13 +1,11 @@
 from robyn import Robyn
 
-from handler.user_cache_handler import userCache
 from service_user.controller.v1 import user_controller as v1
 
 
 def setup_routes(app: Robyn):
     @app.get("/api/user/health")
     async def health_check(request):
-        await userCache.init_cache_handler()
         return {"status": "OK"}
 
     v1.UserController(app)
